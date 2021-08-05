7b3a017379f1715ee93013c0de45fcaab39241aa
ymymJeong <ymym@kakao.com>
2021-08-05 10:12:00 +0900

    Initial commit

---
+++ src/main/java/com/ymym/vue_project/VueProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ymym.vue_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ymym/vue_project/VueProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ymym.vue_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
