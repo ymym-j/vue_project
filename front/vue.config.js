ec9fc1f8c7ed4502bcd8ac07f804f6f1780acd36
ymymJeong <ymym@kakao.com>
2021-08-09 17:07:46 +0900

    설정 추가

---
+++ front/vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    outputDir: '../src/main/resources/static',
+    devServer: {
+        port: 3030,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000'
+            }
+        }
+    }
+}
